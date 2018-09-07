ceab089b20125772338a0281b7bf25f2c3b0abbb
Mustafa Sidiqi <mustafasidiqi95@gmail.com>
2018-09-07 17:58:12 +0200

    ngSwitch

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-test]',
+  selector: 'app-test',
   template:  `
 
   <div *ngIf="display; then thenBlock; else elseBlock"></div>
