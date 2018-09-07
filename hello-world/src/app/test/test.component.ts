b985df53e521fe99686dbf2d2eae202a639aca54
Mustafa Sidiqi <mustafasidiqi95@gmail.com>
2018-09-07 17:42:16 +0200

    ngif directive

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  selector: '[app-test]',
+  template:  `
+
+  <div *ngIf="display; then thenBlock; else elseBlock"></div>
+
+  <ng-template #thenBlock>
+    <h2>
+      Welcome {{name}}
+    </h2>
+  </ng-template>
+
+  <ng-template #elseBlock>
+    <h2>
+      Name is hidden
+    </h2>
+  </ng-template>
+
+  <h2 [class]="successClass">
+  {{name.length}}
+  </h2>
+  <input [(ngModel)]="name" type="test">
+  <button (click)="onClick(input.value)">Greet</button>
+  {{name}}
+  `,
+  styles: [`
+  .text-success {
+    color: green;
+  }
+  .text-danger {
+    color: red;
+  }
+  .text-special {
+    font-style: italic;
+  }
+  `]
 })
 export class TestComponent implements OnInit {
-
+  public name = "";
+  public display = false;
+  public successClass = "text-danger";
   constructor() { }
 
   ngOnInit() {
+  }
+
+  greetUser() {
+    return "Hello " + this.name;
+  }
+
+  onClick(input) {
+    console.log(input);
   }
 
 }
